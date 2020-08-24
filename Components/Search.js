8403d3d16f6e076a25ce7dc7165ed8e3d9613ece
Olivier FOSTIER <olivier.fostier@groupeseloger.com>
2020-08-24 16:28:17 +0200

    * Use keyword from inputtext to process search * Retrieve film list from API * Display result with custom CSS * optimize state use

--- Components/Search.js
+++ Components/Search.js
@@ -1,20 +1,51 @@
 import React from 'react'
 import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
-import films from "../Helpers/filmsData";
+//import films from "../Helpers/filmsData";
+import FilmItem from "./FilmItem";
+import {getFilmsFromApiWithSearchedText} from "../API/TMDPApi";
 
 class Search extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.searchedText = ""
+        this.state = {
+            films: [],
+
+        }
+    }
+
+    _searchTextInputChanged(text) {
+        //console.log("changed text")
+        //this.setState({ searchedText: text })
+        this.searchedText = text
+    }
+
+    _loadFilms() {
+        console.log(this.searchedText) // Un log pour vérifier qu'on a bien le texte du TextInput
+        if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+            getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+                this.setState({ films: data.results })
+            })
+        }
+    }
+
     render() {
-        console.log({films})
+        console.log("RENDER")
         return (
             // Ici on rend à l'écran les éléments graphiques de notre component custom Search
             <View style={styles.main_container}>
-                <TextInput style={styles.textinput} placeholder='Titre du film'/>
-                <Button style={styles.textinput} title='Rechercher' onPress={() => {}}/>
+                <TextInput
+                    style={styles.textinput}
+                    placeholder='Titre du film'
+                    onChangeText={(text) => this._searchTextInputChanged(text)}
+                />
+                <Button style={styles.textinput} title='Rechercher' onPress={() => this._loadFilms()}/>
                 <FlatList
                     //data={[{key: '1', title: 'Rudra'}, {key: '2', title: 'Shiva'}, {key: '3', title: 'Parvati'}]}
-                    data={films}
+                    data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) => <Text>{item.title}</Text>}
+                    renderItem={({item}) => <FilmItem film={item}/>}
                 />
             </View>
         )
--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Run with EXPO
 
 Features :
 
-v0.1
+**v0.1.1**
+* Use keyword from inputtext to process search
+* Retrieve film list from API
+* Display result with custom CSS
+* optimize state use
+
+**v0.1**
 * Display a short FLATLIST from data file
 
