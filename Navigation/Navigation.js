ac926761bf836b8e7ca8b51ce229d4e82467ae73
Olivier FOSTIER <olivier.fostier@groupeseloger.com>
2020-08-25 16:50:18 +0200

    Add custom Tab navigation and access to Favorites page

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,22 +1,63 @@
 //import {createStackNavigator, createAppContainer} from "react-navigation-stack"
+import React from 'react'
+import { StyleSheet, Image } from 'react-native';
 import {createStackNavigator} from "react-navigation-stack";
 import {createAppContainer} from "react-navigation";
+import {createBottomTabNavigator} from "react-navigation-tabs";
 import Search from "../Components/Search"
 import FilmDetail from "../Components/FilmDetail";
+import Favorites from "../Components/Favorites";
 
 const SearchStackNavigator = createStackNavigator({
-    Search: { // Ici j'ai appelé la vue "Search" mais on peut mettre ce que l'on veut. C'est le nom qu'on utilisera pour appeler cette vue
+    Search: {
         screen: Search,
         navigationOptions: {
-            title: 'Search'
+            title: 'Rechercher'
         }
     },
     FilmDetail: {
-        screen: FilmDetail,
-        navigationOptions: {
-            title: 'Film Detail'
-        }
+        screen: FilmDetail
     }
 })
 
-export default createAppContainer(SearchStackNavigator)
+const MoviesTabNavigator = createBottomTabNavigator(
+    {
+        Search: {
+            screen: SearchStackNavigator,
+            navigationOptions: {
+                tabBarIcon: () => { // On définit le rendu de nos icônes par les images récemment ajoutés au projet
+                    return <Image
+                        source={require('../Images/ic_search.png')}
+                        style={styles.icon}/> // On applique un style pour les redimensionner comme il faut
+                }
+            }
+        },
+        Favorites: {
+            screen: Favorites,
+            navigationOptions: {
+                tabBarIcon: () => {
+                    return <Image
+                        source={require('../Images/ic_favorite.png')}
+                        style={styles.icon}/>
+                }
+            }
+        }
+    },
+    {
+        tabBarOptions: {
+            activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+            inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+            showLabel: false, // On masque les titres
+            showIcon: true // On informe le TabNavigator qu'on souhaite afficher les icônes définis
+        }
+    }
+)
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 30,
+        height: 30
+    }
+})
+
+export default createAppContainer(MoviesTabNavigator)
--- README.md
+++ README.md
@@ -8,15 +8,23 @@ with the capacity to show details for a film and add it to your favorite list.
 Run with EXPO
 
 ##Resources
-[Tutorial From OPENCLASSROOMS website](https://openclassrooms.com/fr/courses/4902061-developpez-une-application-mobile-react-native/4902068-decouvrez-le-developpement-mobile-actuel)
+[Tutorial From **OPENCLASSROOMS** website](https://openclassrooms.com/fr/courses/4902061-developpez-une-application-mobile-react-native/4902068-decouvrez-le-developpement-mobile-actuel)
 
 ##Features 
 
 **v0.2.0**
-* Function add to Favorites (using REDUX)
+* Function add to Favorites (using **REDUX**)
+* Add favorite page with custom navigation (**TabNavigator**)
 - npm install --save redux
 - npm install --save react-redux
+- npm install --save react-navigation-tabs
+- npm install --save react-native-reanimated
 
+**Caution** 
+Have to fix some compilation issues whith :
+- expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+and
+- expo start -c ( -c : to clear cache)
 
 **v0.1.4**
 * Add Film Detail Navigation screen with **StackNavigator**
